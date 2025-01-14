a05fed2d31ab19931315f11421acbecb945766bb
Victor <victorlima0302@gmail.com>
2025-01-14 18:23:33 -0300

    feat: adiciona os itens da sidebar

---
+++ src/pages/notificacoes.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/template/Layout";
+
+export default function Notificacao() {
+    return (
+        <div>
+            <Layout
+                titulo="Notificações"
+                subtitulo="subtitulo pagina Notificações"
+            >
+                <h3>Conteudo</h3>
+            </Layout>
+        </div>
+    );
+}
