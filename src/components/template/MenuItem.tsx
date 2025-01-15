a5542608c70453ec902fadb3898e911f47a5f733
Victor <victorlima0302@gmail.com>
2025-01-15 10:09:49 -0300

    feat: modifica o menuItem e o menuLateral para ter a opção Sair

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,24 +1,34 @@
 import Link from "next/link";
 
 interface MenuItemProps {
-    url: string;
+    url?: string;
+    onClick?: (evento: any) => void;
+    className?: string;
     texto: string;
     icone: any;
 }
 
 export default function MenuItem(props: MenuItemProps) {
+    function renderizarLink() {
+        return (
+            <div
+                className={`flex flex-col justify-center items-center h-20 w-20 text-gray-600 ${props.className}`}
+            >
+                {props.icone}
+                <span className={`text-xs font-light `}>{props.texto}</span>
+            </div>
+        );
+    }
     return (
-        <li className={`hover:bg-gray-100 `}>
-            <Link href={props.url}>
-                <div
-                    className={`flex flex-col justify-center items-center h-20 w-20`}
-                >
-                    {props.icone}
-                    <span className={`text-xs font-light text-gray-600`}>
-                        {props.texto}
-                    </span>
-                </div>
-            </Link>
+        <li
+            onClick={props.onClick}
+            className={`hover:bg-gray-100 cursor-pointer`}
+        >
+            {props.url ? (
+                <Link href={props.url}>{renderizarLink()}</Link>
+            ) : (
+                renderizarLink()
+            )}
         </li>
     );
 }
--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,22 +1,32 @@
-import { IconeAjustes, IconeCasa, IconeSino } from "../icons";
+import { IconeAjustes, IconeCasa, IconeSair, IconeSino } from "../icons";
 import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function MenuLateral() {
     return (
-        <aside>
+        <aside className={`flex flex-col`}>
             <div
                 className={`flex flex-col items-center justify-center h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800`}
             >
                 <Logo />
             </div>
-            <ul>
+            <ul className={`flex-grow`}>
                 <MenuItem url="/" texto="Início" icone={IconeCasa} />
                 <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
                 <MenuItem
                     url="/notificacoes"
                     texto="Notificações"
                     icone={IconeSino}
+                />
+            </ul>
+            <ul>
+                <MenuItem
+                    onClick={() => console.log("logout")}
+                    texto="Sair"
+                    icone={IconeSair}
+                    className={`
+                        text-red-600 hover:bg-red-400 hover:text-white
+                        `}
                 />
             </ul>
         </aside>
