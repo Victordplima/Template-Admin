0895a026f03c0fc8dc3a4701245125e26ece13ee
Victor <victorlima0302@gmail.com>
2025-01-16 12:16:53 -0300

    fix: tira o botão placeholder do notificações

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,17 +1,12 @@
 import Layout from "@/components/template/Layout";
-import useAppData from "@/data/hook/useAppData";
 
 export default function Notificacao() {
-    const ctx = useAppData();
-
     return (
         <div>
             <Layout
                 titulo="Notificações"
                 subtitulo="subtitulo pagina Notificações"
-            >
-                <button onClick={ctx.alternarTema}>dwaadwaw</button>
-            </Layout>
+            ></Layout>
         </div>
     );
 }
