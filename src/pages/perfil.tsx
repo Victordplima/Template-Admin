7915f9ec37f3a3fefc63e844e8c04d0cd22c11fc
Victor <victorlima0302@gmail.com>
2025-01-26 17:56:23 -0300

    feat: adiciona o avatar do usuario

---
+++ src/pages/perfil.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/template/Layout";
+
+export default function Perfil() {
+    return (
+        <div>
+            <Layout
+                titulo="Perfil"
+                subtitulo="subtitulo pagina Perfil"
+            ></Layout>
+        </div>
+    );
+}
